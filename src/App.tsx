9720e60dbb2135194c987c23d5f5c3efecd4bf04
Waldemar <waldemar.penner91@gmail.com>
2019-07-23 10:33:35 +1000

    add app header

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,53 @@ import bg from "./app-bg.jpg";
 
 const App: React.FC = () => {
   return (
-    <div
-      className={css`
-        display: flex;
-        height: 100vh;
-        width: 100vw;
-        align-items: center;
-        justify-content: center;
-      `}
-    >
-      <div
+    <>
+      <h1
         className={css`
-          background-image: url(${bg});
-          background-size: cover;
-          position: fixed;
-          z-index: 1;
-          display: block;
-          width: 100%;
-          height: 100%;
-        `}
-      />
-      <Card
-        className={css`
-          border-top: 3px solid ${colors.blue500};
-          z-index: 100;
+          font-family: "Dancing Script", cursive;
+          font-size: 4.8rem;
+          color: ${colors.blue700};
+          position: absolute;
+          top: 4.8rem;
+          right: 4.8rem;
+          z-index: 1000;
         `}
       >
-        <Router>
-          <PollCreation path="/" />
-          <PollParticipation path="/poll/:pollId" />
-        </Router>
-      </Card>
-    </div>
+        Plan it!
+      </h1>
+      <div
+        className={css`
+          display: flex;
+          height: 100vh;
+          width: 100vw;
+          align-items: center;
+          justify-content: center;
+        `}
+      >
+        <div
+          className={css`
+            background-image: url(${bg});
+            background-size: cover;
+            position: fixed;
+            z-index: 1;
+            display: block;
+            width: 100%;
+            height: 100%;
+          `}
+        />
+        <Card
+          className={css`
+            border-top: 3px solid ${colors.blue500};
+            z-index: 100;
+          `}
+        >
+          <Router>
+            <PollCreation path="/" />
+            <PollParticipation path="/poll/:pollId" />
+          </Router>
+        </Card>
+      </div>
+    </>
   );
 };
 
